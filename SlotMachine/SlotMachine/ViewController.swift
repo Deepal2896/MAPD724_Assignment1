261dd1bdc30107ddf545b833ea58699442ae5485
Deepal2896 <72115125+Deepal2896@users.noreply.github.com>
2021-02-01 02:16:45 -0500

    Add files via upload

---
+++ SlotMachine/SlotMachine/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SlotMachine
+//
+//  Created by Deepal Patel, Mansi Majethia on 2021-01-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
